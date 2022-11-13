15c1b18c767cfbb8912a699f8952ad5642717888
Prajwal Raj Basnet <44965674+Prajwalrajbasnet@users.noreply.github.com>
2022-11-14 00:59:33 +0545

    Create backups of existing dotfiles before replacing them

--- setup.sh
+++ setup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# create symlinks
-echo "Creating symlinks..."
-ln -s -r .zshrc ~/.zshrc
-ln -s -r .tmux.conf ~/.tmux.conf
-ln -s -r .ssh/config ~/.ssh/config
+# Backups existing dotfiles to ~/.dotfiles_old and creates symlinks to the new ones
+
+# Variables
+old_dir=~/.dotfiles_old
+files=".zshrc .tmux.conf .profile .gitconfig .ssh/config"
+
+# Create .dotfiles_old directory in home
+echo "Creating $old_dir for backup of any existing dotfiles in ~"
+mkdir -p $old_dir
+echo "...done"
+
+# move existing dotfiles to old dir and create symlinks
+for $file in $files; do
+  echo "Moving $file to $old_dir"
+  mv -p $file $old_dir/
+  
+  echo "Creating symlink for $file from the dotfiles"
+  ln -s -r $file ~/$file
+done
